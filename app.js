9199196d10ea0a6e504240daab2c71c21dafc2b4
Tallstone1984 <97644848+Tallstone1984@users.noreply.github.com>
2022-01-13 14:57:22 -0600

    updated Bug-Tracker and changed styles.

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-
-
 function addBug() {
-  var input = document.getElementById("input");
-  var newBug = input.value;
-  var assigned = document.getElementById("inputAssigned");
-//   var selected = assigned.options[assigned.selectedIndex].value;
-//   console.log(selected)
-  var severity = document.getElementById("inputSeverity");
-  console.log(severity.value)
-  if (newBug != "") {
-    var item = document.createElement("li");
-
+    var input = document.getElementById("input");
+    var newBug = input.value;
+    var assigned = document.getElementById("inputAssigned");
+    var severity = document.getElementById("inputSeverity");
+    console.log(severity.value);
+    if (newBug != "") {
+        var item = document.createElement("li");
 
     item.innerHTML =
       '<input type="button" id="low_marker" class="important" onclick="important(this.parentNode)" value=" ! " />' +
-     severity.value +" "+ newBug + " (" + assigned.value + ") "+
+      severity.value + " " + newBug + " (" + assigned.value + ") " +
       '<input type="button" class="done" onclick="markDone(this.parentNode)" value="complete" /> ' +
-      '<input type="button" class="remove" onclick="remove(this.parentNode)" value="remove" /> ' ;
-      document.getElementById("tasks").appendChild(item);
+      '<input type="button" class="remove" onclick="remove(this.parentNode)" value="remove" /> ';
+    document.getElementById("tasks").appendChild(item);
 
     input.value = "";
     input.placeholder = "enter issue...";
-  }
+    }
 }
 
 function important(item) {
@@ -33,13 +28,5 @@ function markDone(item) {
 function remove(item) {
   if (item.className == "finished") {
     item.remove();
-    //Alternative solution
-    // document.getElementById(remove).disabled = true;
   }
-}
-function doAbout() {
-//   document.getElementById("divabout").innerHTML = "Author Colleen Lohr";
-}
-function clearAbout() {
-  document.getElementById("divabout").innerHTML = "";
 }
