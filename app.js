ae9ed2fb775f2827a675f44aca9648c59db5c17a
Colleen Lohr <cmlohr@outlook.com>
2022-01-12 22:25:57 -0600

    changed to codepen repo

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-function fetchIssues() {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesList = document.getElementById('issuesList');
+function addBug() {
+  var input = document.getElementById("input");
+  var newTask = input.value;
+  if (newTask != "") {
+    var item = document.createElement("li");
 
-    issuesList.innerHTML = '';
+    item.innerHTML =
+      '<input type="button" class="important" onclick="important(this.parentNode)" value=" ! " />' +
+      '<input type="button" class="done" onclick="markDone(this.parentNode)" value="&#x2713;" /> ' +
+      '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' +
+      newTask;
+    document.getElementById("tasks").appendChild(item);
+
+    input.value = "";
+    input.placeholder = "enter task...";
+  }
+}
+function important(item) {
+  item.className = "important";
+}
+function markDone(item) {
+  item.className = "finished";
+}
+function remove(item) {
+  if (item.className == "finished") {
+    item.remove();
+    //Alternative solution
+    // document.getElementById(remove).disabled = true;
+  }
+}
+function doAbout() {
+  document.getElementById("divabout").innerHTML = "Author Colleen Lohr";
+}
+function clearAbout() {
+  document.getElementById("divabout").innerHTML = "";
 }
