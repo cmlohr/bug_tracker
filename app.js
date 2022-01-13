1f0c769c6559444b36c0bebe32fce223f2ea8005
cmlohr <cmlohr@outlook.com>
2022-01-13 15:53:38 -0600

    updated styles

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ function addBug() {
         var item = document.createElement("li");
 
     item.innerHTML =
-      '<input type="button" id="low_marker" class="important" onclick="important(this.parentNode)" value=" ! " />' +
-      severity.value + " " + newBug + " (" + assigned.value + ") " +
+      '<input type="button" id="low_marker" class="important" onclick="important(this.parentNode)" value=" &#9733; " />' +
+      "&nbsp;<span class='bug_list_item'> Severity:&nbsp;</span>  <span class='bug_list_item'>" +
+      severity.value +
+      "</span>&nbsp;<span class='post'>|</span>&nbsp;<span class='bug_list_item'> Summary:&nbsp;</span><span class='bug_list_item'>" +
+      newBug +
+      "</span>&nbsp;<span class='post'>|</span>&nbsp;<span class='bug_list_item'> Assigned:&nbsp;</span><span class='bug_list_item'>" +
+      assigned.value +
+      "</span>&nbsp;" +
       '<input type="button" class="done" onclick="markDone(this.parentNode)" value="complete" /> ' +
       '<input type="button" class="remove" onclick="remove(this.parentNode)" value="remove" /> ';
     document.getElementById("tasks").appendChild(item);
